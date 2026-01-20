c6fc481074be5dc0f569187738509ca8762288d2
accessaidan <aidan.hedges.470@accesscreative.ac.uk>
2026-01-20 12:40:48 +0000

    idk 2

--- mock.py
+++ mock.py
@@ -1,32 +1,20 @@
-from flask import Flask, render_template, Blueprint
+from flask import Flask, render_template
 from flask import flash, redirect, url_for
-from flask_bootstrap import Bootstrap5
 
+from routes import routes_blueprint
 from database import User, db
 
 
 app = Flask(__name__)
-app.secret_key = 'secret_key'
-Bootstrap5(app)
-
-
-# print(app.jinja_env.globals)  # <--- check if 'bootstrap5' exists
-
-from routes import routes_blueprint
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
 
 db.init_app(app)
 
 
-
-
-
-
-
 with app.app_context():
     db.create_all()
+
 
 app.register_blueprint(routes_blueprint)
 
